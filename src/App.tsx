522c00ed03f2e3ab60b7778182dc0ec758bda2c6
zdeep10 <gabriel.wti02@gmail.com>
2022-08-18 19:50:35 -0300

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cardapio from './pages/Cardapio/Cardapio'
+
+function App() {
+
+
+  return (
+    // MAIN CONTENT
+    <Cardapio />
+    // END MAIN CONTENT
+  )
+}
+
+export default App
---
+++ src/pages/Cardapio/Cardapio.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Section from "./style";
+
+
+const Cardapio = () => {
+    return (
+        <Section>
+            <div className="container h-100">
+                <div className="row h-100 justify-content-center align-items-center">
+                    <div className="text-center">
+                        <h1>Todo App with Typescript</h1>
+                    </div>
+                </div>
+            </div>
+        </Section>
+    )
+}
+
+export default Cardapio;
