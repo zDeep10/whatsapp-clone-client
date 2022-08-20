1957dd00ee834c1fc0f1f070ff1d48e2a88d704f
zdeep10 <gabriel.wti02@gmail.com>
2022-08-20 12:33:59 -0300

    Cleaning project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import Header from './layout/Header/Header'
-import Cardapio from './pages/Cardapio/Cardapio'
 
 function App() {
   return (
     <>
-      {/* HEADER */}
-      <Header />
-      {/* END HEADER */}
-
-      {/* MAIN CONTENT */}
-      <Cardapio />
-      {/* END MAIN CONTENT */}
+      <div>Component</div>
     </>
   )
 }
