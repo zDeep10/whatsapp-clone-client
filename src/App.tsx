51df674b6bb63e02ceb5bd43783d4fa299018d09
zdeep10 <gabriel.wti02@gmail.com>
2022-08-29 20:12:40 -0300

    Implementing React Rounter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Chat from './pages/Chat/Chat'
 import Login from './pages/Login/Login'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Login />
-      {/* <Chat /> */}
-    </>
+    <Routes>
+      <Route path='/' element={<Login />}>
+      </Route>
+      <Route path='/chat' element={<Chat />}>
+      </Route>
+    </Routes>
   )
 }
 
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "../../components/Form/Form";
 import { Background, Col } from "./style";
+import UsersData from "../../data/users.json"
 
 const Login = () => {
 
